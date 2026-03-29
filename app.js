3510f2ec6d1c82dac8821ce41ab8c1d141573ad4
adminz <adminz@devopsclass.local>
2026-03-29 23:09:14 +0200

    Initial working app

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send(`
+        <h1>🚀 DevOps Live App</h1>
+        <p>Welcome students! This app is deployed using DevOps.</p>
+    `);
+});
+
+app.listen(3000, () => {
+    console.log("Server running on http://localhost:3000");
+});
